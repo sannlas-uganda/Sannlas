e9b00f843d5f5e0bec47d06c524448f1cc839645
sannlas-uganda <ndyomugishasteven@gmail.com>
2026-08-20 23:41:00 -0700

    Add render_template for serving HTML files

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, send_from_directory
+from flask import Flask, jsonify, request, send_from_directory,render_template
 from flask_cors import CORS
 import os
 from datetime import datetime, timedelta
