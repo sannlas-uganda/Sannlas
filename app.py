ee9d9da790c56cbfc3d42fafa13cbcc9705e2fe2
sannlas-uganda <ndyomugishasteven@gmail.com>
2026-09-10 21:13:48 -0700

    Update app.py

--- app.py
+++ app.py
@@ -407,8 +407,8 @@ def admin_save_billboard():
     cfg['active'] = bool(data.get('active', cfg.get('active', False)))
     cfg['text'] = data.get('text', cfg.get('text',''))[:200]
     cfg['link'] = data.get('link', cfg.get('link',''))[:300]
-        cfg['media_url'] = data.get('media_url', cfg.get('media_url', ''))
-        cfg['type'] = data.get('type', cfg.get('type', 'image'))
+    cfg['media_url'] = data.get('media_url', cfg.get('media_url', ''))
+    cfg['type'] = data.get('type', cfg.get('type', 'image'))
         if 'duration' in data:
             dur = str(data.get('duration', ''))
             if dur == "0":
