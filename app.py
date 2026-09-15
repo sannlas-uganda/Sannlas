ebe63628359ce6ba86d037fc8c08205858114bb4
sannlas-uganda <ndyomugishasteven@gmail.com>
2026-09-15 09:39:05 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1493,7 +1493,7 @@ def get_shop_by_slug(slug):
             products = load_db('products.json', [])
             shop_products = [p for p in products if p.get('shop_slug')==slug][:100]
                 return jsonify({'success':True,'shop':shop,'products':shop_products})
-    except Exception as e:
+        except Exception as e:
         print("get_shop_by_slug error:", e)
         return jsonify({'success':False,'message':'Server busy, try again'}),500
 
