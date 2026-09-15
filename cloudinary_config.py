7039e92c84b9b09080ca225fac9b5766cdd77e76
sannlas-uganda <ndyomugishasteven@gmail.com>
2026-09-15 05:53:50 -0700

    Create cloudinary_config.py

---
+++ cloudinary_config.py
@@ -0,0 +1,9 @@
+import cloudinary
+import cloudinary.uploader
+
+cloudinary.config(
+  cloud_name = "y9z9p6ws",
+  api_key = "486335228977968",
+  api_secret = "a5k4QK85iL9h_rGFLK6QwybVLC4",
+  secure = True
+)
