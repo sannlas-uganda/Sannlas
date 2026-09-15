82f587b734e88e97fcdca0423d60fa7460e2435b
sannlas-uganda <ndyomugishasteven@gmail.com>
2026-09-15 06:45:20 -0700

    Update cloudinary_config.py

--- cloudinary_config.py
+++ cloudinary_config.py
@@ -2,7 +2,7 @@ import cloudinary
 import os
 
 cloudinary.config( 
-  cloud_name = os.environ.get('CLOUDINARY_CLOUD_NAME', 'dy9z9p6ws'), 
+  cloud_name = os.environ.get('CLOUDINARY_CLOUD_NAME', 'dy9z9p6ws'),
   api_key = os.environ.get('CLOUDINARY_API_KEY', '899416258898322'), 
   api_secret = os.environ.get('CLOUDINARY_API_SECRET', 'a5k4QK85iL9h_rGFLK6QwybVLC4'),
   secure = True
