70e6d25b0318f2949a9cb6d45f223143b5ec6a60
sannlas-uganda <ndyomugishasteven@gmail.com>
2026-09-15 09:19:55 -0700

    Update greeting from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -1494,8 +1494,8 @@ def get_shop_by_slug(slug):
             shop_products = [p for p in products if p.get('shop_slug')==slug][:100]
 
         return jsonify({'success':True,'shop':shop,'products':shop_products})
-          except Exception as e:
-             print("get_shop_by_slug error:", e)
+            except Exception as e:
+                print("get_shop_by_slug error:", e)
         return jsonify({'success':False,'message':'Server busy, try again'}),500
 
 @app.route('/api/shop/<slug>')
